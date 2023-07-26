4b3906a58cf738520566fb49b8db5d9258515e48
Mohamed Nagy <mohmmednagy@gmail.com>
2023-07-26 09:31:16 +0300

    first commit

---
+++ file1.c
@@ -0,0 +1,9 @@
+int numOne = 10;
+const int numTwo = 28;
+const int numThree;
+
+void swapping(void)
+{
+	int localNumOne = 6;
+	static int localNumTwo = 20;	
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+int varOne = 4;
+const int varTwo = 1;
+int varThree;
+
+void main(void)
+{
+	int localVarOne = 3;
+	static int localVarTwo = 5;	
+}
